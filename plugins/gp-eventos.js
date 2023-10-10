a9be2cb55ea99daff61512da3bf2fd24f2cb2bbf
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-10 13:53:08 -0300

    Update gp-eventos.js

--- plugins/gp-eventos.js
+++ plugins/gp-eventos.js
@@ -1,8 +1,9 @@
 let handler = async (m, { conn }) => {
 m.reply(global.eventos)}
 handler.help = ['eventos']
-handler.tags = ['grupo']
+handler.tags = ['Futabu Club']
 handler.command = /^(evento|eventos|eventofutabuclub)$/i
+handler.group = true
 export default handler
 
 global.eventos = `Nada aún!`
