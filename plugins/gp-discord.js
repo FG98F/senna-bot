216fef2beb707ca8e14f81597ec4dfa2fc141b4c
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-10 13:53:29 -0300

    Update gp-discord.js

--- plugins/gp-discord.js
+++ plugins/gp-discord.js
@@ -1,8 +1,9 @@
 let handler = async (m, { conn }) => {
 m.reply(global.discord)}
 handler.help = ['discord']
-handler.tags = ['grupo']
+handler.tags = ['Futabu Club']
 handler.command = /^(serverdis|serverdiscord|discord|grupodiscord|linkdiscord)$/i
+handler.grouop = true
 export default handler
 
 global.discord = `Nuestro Server de Discord!
