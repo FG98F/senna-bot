ef7a01cbde0abc4528bb44275e2f33a006ab49cb
Colapsus <106274886+ColapsusHD@users.noreply.github.com>
2023-10-10 13:53:53 -0300

    Update gp-concurso.js

--- plugins/gp-concurso.js
+++ plugins/gp-concurso.js
@@ -1,8 +1,9 @@
 let handler = async (m, { conn }) => {
 m.reply(global.concurso)}
 handler.help = ['concurso']
-handler.tags = ['grupo']
+handler.tags = ['Futabu Club']
 handler.command = /^(concurso|concursofutabuclub)$/i
+handler.group = true
 export default handler
 
 global.concurso = `Nada aún!`
