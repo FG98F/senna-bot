73592976f50873ace9397b829e23117e1a23bc66
Brayan Jesus Jurado Félix <90229390+Millonarcito@users.noreply.github.com>
2023-10-15 17:49:00 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['51900000000', 'FX', true],
+  ['51900000000']
 ] //Numeros de owner 
 
 global.mods = [''] 
