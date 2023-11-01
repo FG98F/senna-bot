172c842cd6a70f9f1e3636c3baf87d0dc04f0c89
FG98 <81941690+FG98F@users.noreply.github.com>
2023-11-01 18:45:05 -0300

    Update img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -1,10 +1,36 @@
 
-let handler = async(m, { conn, usedPrefix, command }) => {
-	
-	let img = await conn.getFile(global.API('fgmods', '/api/img/asupan-la', { }, 'apikey'))
+import fetch from "node-fetch"
+let handler = async (m, { conn, args }) => {
+    
+        let username = [
+    'c.sol_',
+    'nuriaarancibiaa',
+    'laurasofiadepende',
+    'mariana.30027',
+    'kyarak_',
+    'luly.mix',
+    'melodyvalenzuelar',
+    'michitasnow',
+    'iamjossaryvallejos10',
+    'notaestheticallyhannah_',
+    '_marinazarrocaa_'
+  ]
+  let pickuser = username[Math.floor(Math.random() * username.length)]
+  let query = args[0] ? args[0] : pickuser
+  
+  try { 
+  	let res = await fetch(global.API('fgmods', '/api/img/asupan-tt', { username: query }, 'apikey'))
+     let video = await res.json()
+     conn.sendFile(m.chat, video.result, 'vid.mp4', `✅ ${mssg.result}`, m) 
+     m.react(dmoji)
+     
+  	 } catch (error) {
+     let img = await conn.getFile(global.API('fgmods', '/api/img/asupan-la', { }, 'apikey'))
     let asupan = img.data
     conn.sendFile(m.chat, asupan, 'vid.mp4', `✅ ${mssg.result}`, m)
-    m.react(dmoji)
+    m.react('🤓')
+  }
+
 }
 handler.help = ['tvid']
 handler.tags = ['img']
