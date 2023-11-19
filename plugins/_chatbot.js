b34f911c84e907caa2c3bf7c3478cf301e5f0c7f
FG98 <81941690+FG98F@users.noreply.github.com>
2023-11-19 13:48:24 -0300

    Add files via upload

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -10,9 +10,13 @@ if (m.isBaileys && m.fromMe)
       if (!user.chatbot)
         return !0
         try {
-        let api = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=${lang}`)
-        let res = await api.json()
-        m.reply(res.success.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
+        let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `text=${encodeURIComponent(m.text)}&lc=${lang}&key=`
+  })
+  let json = await res.json()
+  m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
       } catch {
         m.reply(`❎ La  api de SimSimi se cayo!!\n\nDesactiva chatbot con */off chatbot*`)
       }
--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -6,10 +6,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `✳️ ${mssg.notext}`
   m.react('🗣️') 
   try { 
-  //let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "es" }, ''))
-  let res = await fetch(`https://api.simsimi.net/v2/?text=${text}&lc=${lang}`)
+  //let res = await fetch(`https://api.simsimi.vn/v2/?text=${text}&lc=${lang}`)
+  let res = await fetch('https://api.simsimi.vn/v1/simtalk', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: `text=${encodeURIComponent(text)}&lc=${lang}&key=`
+  })
   let json = await res.json()
-  m.reply(json.success.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
+  m.reply(json.message.replace('simsimi', `${botName}`).replace('Simsimi', `${botName}`).replace('sim simi', `${botName}`))
 } catch {
   m.reply(`❎ Intenta de nuevo mas tarde La api de SimSimi se cayo`)
 }
