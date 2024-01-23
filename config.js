e49289bfb266c69646dba75815a5637b25969193
Fangulito12 <92560987+Fangulito12@users.noreply.github.com>
2024-01-23 10:56:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['595986102574', 'FG98', true],
+  ['595986102574']
 ] //Numeros de owner 
 
 global.mods = [''] 
