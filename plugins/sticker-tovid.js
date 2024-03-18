0499d6e1a02f58f53185a66c9006510825f25dc0
FG98 <81941690+FG98F@users.noreply.github.com>
2024-03-18 14:51:41 -0300

    Update sticker-tovid.js

--- plugins/sticker-tovid.js
+++ plugins/sticker-tovid.js
@@ -1,27 +1,42 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
-import { ffmpeg } from '../lib/converter.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import { promisify } from 'util'
+import { exec as execCallback } from 'child_process'
 
-let handler = async (m, { conn }) => {
-    if (!m.quoted) throw `✳️ ${mssg.replyStick}`
-    let mime = m.quoted.mimetype || ''
-    if (!/webp|audio/.test(mime)) throw `✳️ ${mssg.replyStick}`
-    let media = await m.quoted.download()
-    let out = Buffer.alloc(0)
-    if (/webp/.test(mime)) {
-        out = await webp2mp4(media)
-    } else if (/audio/.test(mime)) {
-        out = await ffmpeg(media, [
-            '-filter_complex', 'color',
-            '-pix_fmt', 'yuv420p',
-            '-crf', '51',
-            '-c:a', 'copy',
-            '-shortest'
-        ], 'mp3', 'mp4')
+const exec = promisify(execCallback)
+
+const converter = async bufferImage => {
+  try {
+    let pathFile = "./tmp/" + ~~(Math.random() * 1000000 + 1) + ".webp"
+    fs.writeFileSync(pathFile, bufferImage)
+    await exec(`convert ${pathFile} ${pathFile}.gif`)
+    await exec(`ffmpeg -i ${pathFile}.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ${pathFile}.mp4`)
+    if (!fs.existsSync(pathFile + ".gif") || !fs.existsSync(pathFile + ".mp4")) {
+      throw new Error("Failed convert file!");
     }
-    await conn.sendFile(m.chat, out, 'tovid.mp4', `*✅ ${mssg.result}*`, m)
+    let videoBuffer = fs.readFileSync(pathFile + ".mp4")
+    fs.unlinkSync(pathFile)
+    fs.unlinkSync(pathFile + ".gif")
+    fs.unlinkSync(pathFile + ".mp4")
+    return videoBuffer;
+  } catch(error) {
+    throw error;
+  }
+}
+
+let handler = async (m, { conn, command, usedPrefix }) => {
+	m.react(rwait)
+	if (m.quoted && /sticker/.test(m.quoted.mtype) && !m.quoted.isAnimated) {
+		let img = await m.quoted.download()
+		await conn.sendMessage(m.chat, { image: img, jpegThumbnail: img }, { quoted: m })
+	} else if (m.quoted && /sticker/.test(m.quoted.mtype) && m.quoted.isAnimated) {		
+		let img = await m.quoted.download()
+		let out = await converter(img)
+		await conn.sendMessage(m.chat, { video: out, gifPlayback: /gif/i.test(m.text), gifAttribution: ~~(Math.random() * 2) }, { caption: '*DONE*', quoted: m });
+	} else throw `✳️ ${mssg.replyStick}`
 }
 handler.help = ['tovid']
 handler.tags = ['sticker']
-handler.command = ['tovideo', 'tovid', 'mp4']
+handler.command = ['tovideo', 'tovid', 'mp4', 'tomp4', 'togif']
 
 export default handler
