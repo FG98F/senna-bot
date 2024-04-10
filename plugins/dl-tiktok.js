d40966fd35b274a56ff983cddaa35d9efc4ffdc3
FG98 <81941690+FG98F@users.noreply.github.com>
2024-04-10 12:11:19 -0300

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -6,38 +6,40 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
         if (!args[0]) throw `✳️ ${mssg.noLink('TikTok')}\n\n 📌 ${mssg.example} : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
         if (!args[0].match(/tiktok/gi)) throw `❎ ${mssg.noLink('TikTok')}`
         m.react(rwait)
-      try {
-        let res = await fetch(global.API('fgmods', '/api/downloader/tiktok2', { url: args[0] }, 'apikey'))
+      
+        try {
+        let res = await fetch(global.API('fgmods', '/api/downloader/tiktok', { url: args[0] }, 'apikey'))
         let data = await res.json()
 
-        if (data.result.video) {
+        if (!data.result.images) {
             let tex = `
 ┌─⊷ *TIKTOK DL* 
-▢ *${mssg.name}:* ${data.result.author.name}
+▢ *${mssg.name}:* ${data.result.author.nickname}
 ▢ *${mssg.username}:* ${data.result.author.unique_id}
-▢ *${mssg.duration}:* ${data.result.video.durationFormatted}
-▢ *${mssg.quality}:* ${data.result.video.ratio}
-▢ *Likes:* ${data.result.stats.likeCount}
-▢ *${mssg.views}:* ${data.result.stats.playCount}
+▢ *${mssg.duration}:* ${data.result.duration}
+▢ *Likes:* ${data.result.digg_count}
+▢ *${mssg.views}:* ${data.result.play_count}
 ▢ *${mssg.desc}:* ${data.result.title}
 └───────────
 `
-            conn.sendFile(m.chat, data.result.video.noWatermark, 'tiktok.mp4', tex, m);
+            conn.sendFile(m.chat, data.result.play, 'tiktok.mp4', tex, m);
             m.react(done)
         } else {
             let cap = `
-▢ *Likes:* ${data.result.stats.likeCount}
+▢ *Likes:* ${data.result.digg_count}
 ▢ *${mssg.desc}:* ${data.result.title}
 `
-            for (let tt of data.result.images) {
-                conn.sendMessage(m.chat, { image: { url: tt.url }, caption: cap }, { quoted: m })
+            for (let ttdl of data.result.images) {
+                conn.sendMessage(m.chat, { image: { url: ttdl }, caption: cap }, { quoted: m })
             }
-            conn.sendFile(m.chat, data.result.music.play_url, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' })
+            conn.sendFile(m.chat, data.result.play, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' })
             m.react(done)
         }
-    } catch (error) {
+
+      } catch (error) {
         m.reply(`❎ ${mssg.error}`)
     }
+   
 }
 
 handler.help = ['tiktok']
