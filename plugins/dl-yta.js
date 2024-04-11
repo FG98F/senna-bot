8b38e80785190fd0508ff4666c5a29963291925d
FG98 <81941690+FG98F@users.noreply.github.com>
2024-04-11 00:57:52 -0300

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,6 +1,11 @@
 
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+import os from 'os'
 import fg from 'api-dylux'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) throw `✳️ ${mssg.example} :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
   if (!args[0].match(/youtu/gi)) throw `❎ ${mssg.noLink('YouTube')}`
@@ -9,7 +14,10 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
  let q = '128kbps'
  
  try {
-		const yt = await fg.yta(args[0])
+ 
+		//let res = await fetch(global.API('fgmods', '/api/downloader/yta', { url: args[0], quality: q}, 'apikey'))
+		//let yt = await res.json()
+		const yt = await fg.yta(args[0]) 
 		let { title, dl_url, quality, size, sizeB } = yt
 		
 		conn.sendFile(m.chat, dl_url, title + '.mp3', `
@@ -21,13 +29,13 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 		m.react(done)
  	} catch {
   try {
-		let yt = await fg.ytmp3(args[0])
-        let { title, size, sizeB, dl_url } = yt
+  	
+	const { title, dl_url } = await ytmp3(args[0]);
+  
 		conn.sendFile(m.chat, dl_url, title + '.mp3', `
  ≡  *FG YTDL 2*
   
 ▢ *📌${mssg.title}* : ${title}
-▢ *⚖️${mssg.size}* : ${size}
 `.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done)
         } catch {
@@ -42,3 +50,28 @@ handler.command = ['ytmp3', 'fgmp3']
 handler.diamond = false
 
 export default handler
+
+
+
+async function ytmp3(url) {
+    const videoInfo = await ytdl.getInfo(url);
+    const { videoDetails } = videoInfo;
+    const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
+    const thumbnail = thumbnails[0].url;
+    
+    const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
+    const tmpDir = os.tmpdir();
+    const audioFilePath = `${tmpDir}/${title}.mp3`;
+
+    await streamPipeline(audioStream, fs.createWriteStream(audioFilePath));
+
+    return {
+        title,
+        views: viewCount,
+        publish: uploadDate,
+        duration: lengthSeconds,
+        quality: '128kbps',
+        thumb: thumbnail,
+        dl_url: audioFilePath
+    };
+}
