f2b449a1761bfe55890356bffb623cf079329103
FG98 <81941690+FG98F@users.noreply.github.com>
2024-04-12 12:25:21 -0300

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,20 +1,21 @@
 
 import fetch from 'node-fetch'
-import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
 
-   let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
-   let resdl = await res.json()
-    for (let data of resdl.result) {
-    conn.sendFile(m.chat, data.url, 'igdl.mp4', `✅ ${mssg.result}`, m)
-    m.react(done)
-  }
+    let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
+    if (!res.ok) throw `❎ ${mssg.error} ` 
+    let data = await res.json()
+
+    for (let item of data.result) {
+        conn.sendFile(m.chat, item.url, 'igdl.jpg', `✅ ${mssg.result}`, m)
+    }
 }
+
 handler.help = ['instagram <link ig>']
 handler.tags = ['dl']
-handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
+handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid']
 handler.diamond = true
 
 export default handler
