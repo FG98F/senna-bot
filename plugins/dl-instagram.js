31b83477c9f84fd4ca49353aefa15c931e04c97e
FG98 <81941690+FG98F@users.noreply.github.com>
2024-04-12 12:40:01 -0300

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -4,6 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
     m.react(rwait)
 
+try {
     let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
     if (!res.ok) throw `❎ ${mssg.error} ` 
     let data = await res.json()
@@ -11,8 +12,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     for (let item of data.result) {
         conn.sendFile(m.chat, item.url, 'igdl.jpg', `✅ ${mssg.result}`, m)
     }
+  
+  
+    } catch (error) {
+        m.reply(`❎ ${mssg.error}`)
+    }
+    
 }
-
 handler.help = ['instagram <link ig>']
 handler.tags = ['dl']
 handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid']
