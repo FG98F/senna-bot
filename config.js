70aecbbf5acebaada89b378a3a572d4e4817d5c8
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-18 01:14:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5491168352204']
+  ['967733772709', '967733772709', true],
+  ['967733772709']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['967733772709', '967733772709']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
