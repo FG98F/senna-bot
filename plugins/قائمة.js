ab0a948a012445b8236f07e9b2f882582c5846c0
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-18 16:03:28 +0300

    Update قائمة.js

--- plugins/قائمة.js
+++ plugins/قائمة.js
@@ -1,55 +1,55 @@
+```javascript
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    conn.relayMessage(m.chat, {
-      viewOnceMessage: {
-        message: {
-          interactiveMessage: {
-            header: {
-              title: '*قـائـمـة الـاوامــر*'
-            },
-            body: {
-              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
-            },
-            nativeFlowMessage: {
-              buttons: [
-                {
-                  name: 'single_select',
-                  buttonParamsJson: JSON.stringify({
-                    title: 'اضغط هنا ',
-                    sections: [
-                      {
-                        title: '𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕',
-                        highlight_label: '𝙼𝚛 𝙷𝚊𝚖𝚣𝚊',
-                        rows: [
-                          {
-                            header: 'معلومات البوت',
-                            title: '.المطور',
-                            description: '',
-                            id: '/info'
-                          },
-                          {
-                            header: 'قسم الاوامر',
-                            title: '.الاوامر',
-                            description: '',
-                            id: '/menu'
-                          }
-                        ]
-                      }
-                    ]
-                  }),
-                  messageParamsJson: '𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕'
-                }
-              ]
-            }
+  conn.relayMessage(m.chat, {
+    viewOnceMessage: {
+      message: {
+        interactiveMessage: {
+          header: {
+            title: *قـائـمـة الـاوامــر*
+          },
+          body: {
+            text: 🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة
+          },
+          nativeFlowMessage: {
+            buttons: [
+              {
+                name: single_select,
+                buttonParamsJson: JSON.stringify({
+                  title: اضغط هنا,
+                  sections: [
+                    {
+                      title: 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕,
+                      highlight_label: 𝙼𝚛 𝙷𝚊𝚖𝚣𝚊,
+                      image_url: './src/fg_logo.jpg', // Agrega la ruta de la imagen aquí
+                      rows: [
+                        {
+                          header: معلومات البوت,
+                          title: .المطور,
+                          description:  ,
+                          id: /info
+                        },
+                        {
+                          header: قسم الاوامر,
+                          title: .الاوامر,
+                          description:  ,
+                          id: /menu
+                        }
+                      ]
+                    }
+                  ]
+                }),
+                messageParamsJson: 𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕
+              }
+            ]
           }
         }
       }
-    }, {})
-
+    }
+  }, {})
 }
 
-
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['المهام']
-
+handler.help = [ info ]
+handler.tags = [ main ]
+handler.command = [ المهام ]
 export default handler
+```
