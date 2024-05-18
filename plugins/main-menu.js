c49fc670ff975de64d6deb9c476e3d8620eb4f2b
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-18 15:39:38 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -173,3 +173,58 @@ function clockString(ms) {
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
 }
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: '*قـائـمـة الـاوامــر*'
+            },
+            body: {
+              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'اضغط هنا ',
+                    sections: [
+                      {
+                        title: '𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕',
+                        highlight_label: '𝙼𝚛 𝙷𝚊𝚖𝚣𝚊',
+                        rows: [
+                          {
+                            header: 'معلومات البوت',
+                            title: '.المطور',
+                            description: '',
+                            id: '/info'
+                          },
+                          {
+                            header: 'قسم الاوامر',
+                            title: '.الاوامر',
+                            description: '',
+                            id: '/menu'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: '𝑴𝒊𝒓𝒛𝒂 𝑩𝒐𝒕'
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
+
+}
+
+
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['اوامر']
+
+export default handler
