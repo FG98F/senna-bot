c9496a9f7fd566cb8e808bac56dd6e65022a430d
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-18 14:47:19 +0300

    Create قائمة.js

---
+++ plugins/قائمة.js
@@ -0,0 +1,54 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: '*قـائـمـة الـاوامــر*'
+            },
+            body: {
+              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'دوس هنا ',
+                    sections: [
+                      {
+                        title: 'قوائم البوت',
+                        highlight_label: 'اختار',
+                        rows: [
+                          {
+                            header: 'المعلومات',
+                            title: '.معلوماتي',
+                            description: '',
+                            id: 'te'
+                          },
+                          {
+                            header: 'قسم الاوامر',
+                            title: '.الاوامر',
+                            description: '',
+                            id: 'te'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
+
+}
+
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['المهام']
+
+export default handler
