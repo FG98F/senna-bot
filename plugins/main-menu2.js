729ee0ba6fc0dd6fde179813c3a99a8b996fb31e
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-20 01:54:42 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -3,14 +3,14 @@ let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
 ◈ ━━━━ *𝑴𝒊𝒓𝒛𝒂  ┃ ᴮᴼᵀ* ━━━━━ ◈ *${usedPrefix}*
- 👋🏻 مرحباً! 
+ 👋🏻 مرحباً! : %name
  👥 عدد المستخدمين : %totalreg
  🟢 وقت النشاط : %muptime%sbot
 ▢ 
 └──────────────
 `
     let pp = './src/fg_logo.jpg' 
-      conn.sendButton2(m.chat, m2, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+      conn.sendButton2(m.chat, m2, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, pp, [
       ['⦙☰ قائمة الاوامر', `${usedPrefix}اوامر`],
       ['⌬ جروب الدعم', `${usedPrefix}الدعم`]
     ],m, rpyt )
