11884f06e72746032d2ade83f0d37ed73f0580dd
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 17:35:50 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,13 +3,14 @@ let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
 let av = `./src/fg_logo.jpg`  
-let pp =  `./src/fg_logo.jpg`     
+let pp = `./src/fg_logo.jpg` 
+      
       conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
       ['⦙☰ Menu', '/menu'],
       ['⦙☰ Menu 2', '/menu2'],
       [`⌬ ${mssg.gp}s`, '/support']
     ], m, rpyt ) 
-      conn.sendFile(m.chat, av, , 'menu.jpg' , null, m, rpl, true, { type:  'audioMessage' , ptt: true })
+      conn.sendFile(m.chat, av, , 'menu.jpg', null, m, rpl, true, { type:  'audioMessage' , ptt: true })
     m.react( 📃 )
 } 
 
