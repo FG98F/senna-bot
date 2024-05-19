bce94989e5885d6b5d473a52fb6076ef2661fa7e
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 18:09:05 +0300

    Create خاص.js

---
+++ plugins/خاص.js
@@ -0,0 +1,28 @@
+
+let handler = async function (m, { conn, text, usedPrefix }) {
+  
+let m2 = `
+≡ Use estos comandos sin el prefijo: *${usedPrefix}*
+┌─⊷ *AUDIOS* 
+▢ Bot
+▢ Buenos días
+▢ Buenas tardes 
+▢ Buenas noches
+▢ Fino señores
+▢ Sad
+└──────────────
+`
+    let pp = './src/fg_logo.jpg' 
+      conn.sendButton2(m.chat, m2, mssg.ig, pp, [
+      ['⏍ معلومات البوت', `${usedPrefix}botinfo`],
+      ['⌬ جروب الدعم', `${usedPrefix}support`]
+    ],m, rpyt)
+    conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rpl)
+   
+}
+
+handler.help = []
+handler.tags = []
+handler.command = [] 
+
+export default handler
