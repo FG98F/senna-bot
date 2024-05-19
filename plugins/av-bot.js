cffa8b062abd8745b06f79bebabaaa2d3fc37ff2
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 16:43:10 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,23 +1,23 @@
 
-let handler = async (m, { conn }) => {
-  let name = await conn.getName(m.sender);
-  let av = `./src/fg_logo.jpg`;
-  let pp = `./src/fg_logo.jpg`;
-  let buttons = [
-    { buttonId: "/menu", buttonText: { displayText: "⦙☰ Menu" }, type: 1 },
-    { buttonId: "/menu2", buttonText: { displayText: "⦙☰ Menu 2" }, type: 1 },
-    { buttonId: "/support", buttonText: { displayText: `⌬ ${mssg.gp}s` }, type: 1 }
-  ];
-  let message = `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`;
-  await conn.sendFile(m.chat, av, message, mssg.ig, null, m, rpyt, true, { type: "audioMessage", ptt: true });
-  await conn.sendMessage(m.chat, { image: { url: pp }, buttons: buttons }, { quoted: m });
-  m.react("📃");
-};
+let handler = async (m, { conn}) => {
 
-handler.customPrefix = /^(bot|senna)$/i;
-handler.command = new RegExp();
-export default handler;
+let name = conn.getName(m.sender)
+let av = `./src/fg_logo.jpg`  
+let pp =  `./src/fg_logo.jpg `      
+      conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+    ` [ ⦙☰ Menu , /menu ],
+      [ ⦙☰ Menu 2 , /menu2 ],
+   `  [`⌬ ${mssg.gp}s`, /support ]
+    ], m, rpyt) 
+    conn.sendFile(m.chat, av, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig , null, m, rpl, true, { type:  audioMessage , ptt: true })
+   m.react( 📃 )
+} 
+
+handler.customPrefix = /^(bot|senna)$/i
+handler.command = new RegExp
+
+export default handler
 
 function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())];
+  return list[Math.floor(list.length * Math.random())]
 }
