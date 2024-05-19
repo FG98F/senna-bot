db69fb509da49d233f2264258d445f4e92c0d58c
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 14:15:11 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -4,14 +4,13 @@ let handler = async (m, { conn}) => {
 let name = conn.getName(m.sender)
 let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
       
-   let pp = './src/fg_logo.jpg
-
-      conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, pp, [
+    let pp = './src/fg_logo.jpg
+      conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, pp, [
       ['⦙☰ Menu', '/menu'],
       ['⦙☰ Menu 2', '/menu2'],
       [`⌬ ${mssg.gp}s`, '/support']
     ], m) 
-    conn.sendFile(m.chat, av, 'audio.mp3', pp, m, true, { type: 'audioMessage', ptt: true })
+    conn.sendFile(m.chat, pp, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    m.react('📃')
 } 
 
