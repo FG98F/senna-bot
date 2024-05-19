f811097eb4f5ce7f2a59dc18a2ac161f236b1b0d
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 14:40:11 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
-let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`  
+let av = `./src/fg_logo.jpg`  
       
       conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
       ['⦙☰ Menu', '/menu'],
