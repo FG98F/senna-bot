05310567ceba4b1b8f715f3c9b28ce84d695861e
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 18:21:48 +0300

    Update خاص.js

--- plugins/خاص.js
+++ plugins/خاص.js
@@ -2,19 +2,18 @@
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-≡ Use estos comandos sin el prefijo: *${usedPrefix}*
-┌─⊷ *AUDIOS* 
-▢ Bot
-▢ Buenos días
-▢ Buenas tardes 
-▢ Buenas noches
-▢ Fino señores
-▢ Sad
+◈ ━━━━ *𝑴𝒊𝒓𝒛𝒂  ┃ ᴮᴼᵀ* ━━━━ ◈ *${usedPrefix}*
+┌─⊷ *𝑴𝒊𝒓𝒛𝒂* 
+▢ 👋🏻 مرحباً! *%name*
+▢ 👥 عدد المستخدمين : %totalreg
+▢ 🟢 وقت النشاط : %muptime
+%sbot
+▢ 
 └──────────────
 `
     let pp = './src/fg_logo.jpg' 
       conn.sendButton2(m.chat, m2, mssg.ig, pp, [
-      ['⏍ معلومات البوت', `${usedPrefix}botinfo`],
+      ['⏍ قائمة الاوامر', `${usedPrefix}menu`],
       ['⌬ جروب الدعم', `${usedPrefix}support`]
     ],m, rpyt)
     conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rpl)
