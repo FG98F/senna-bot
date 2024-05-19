779aa41bc3c32b90a4b54a8077a36a85c1cdf591
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 11:08:18 +0300

    Update apps.js

--- plugins/apk4.js
+++ plugins/apk4.js
@@ -15,126 +15,152 @@ let handler = async (m, {
     ]
 
     let [feature, inputs, inputs_, inputs__, inputs___] = text.split("|")
-    if (!lister.includes(feature)) return m.reply("*Example:*\n.apk4all search|vpn\n\n*Pilih type yg ada*\n" + lister.map((v, index) => "  ○ " + v).join("\n"))
+    if (!lister.includes(feature)) return m.reply("ex : apps search|lite \n" + lister.map((v, index) => "  ○ " + v).join("\n"))
 
     if (lister.includes(feature)) {
 
         if (feature == "search") {
-            if (!inputs) return m.reply("Input query link\nExample: .apk4all search|vpn")
+            if (!inputs) return m.reply("ex : \n .apps search|lite")
             await m.reply(wait)
             try {
-                let res = await searchApk4all(inputs)
+                let res = await searchHippo(inputs)
                 let teks = res.map((item, index) => {
                     return `🔍 *[ RESULT ${index + 1} ]*
 
-📰 *Title:* ${item.title}
-🔗 *Url:* ${item.titleUrl}
-🖼️ *Image:* ${item.imageUrl}
-⭐️ *Rating:* ${item.rating}
-👀 *Views:* ${item.views}
-📊 *Average:* ${item.average}
-📅 *Date:* ${item.updateDate}`
-
+📌 *title:* ${item.title}
+🔗 *link:* ${item.link}
+🖼️ *image:* ${item.image}
+🎯 *programTitle:* ${item.programTitle}
+🔗 *programLink:* ${item.programLink}
+👨‍💻 *developer:* ${item.developer}
+📄 *license:* ${item.license}
+📃 *summary:* ${item.summary}`
                 }).filter(v => v).join("\n\n________________________\n\n")
                 await m.reply(teks)
             } catch (e) {
                 await m.reply(eror)
             }
         }
-
         if (feature == "app") {
-            if (!inputs) return m.reply("Input query link\nExample: .apk4all app|link")
+            if (!inputs) return m.reply("Input query link\nExample: .filehippo search|vpn")
+            await m.reply(wait)
             try {
-                let resl = await getApk4all(inputs)
-                
-                let cap = `📌 *Title:* ${resl.info.title}
-🔖 *Version:* ${resl.info.currentVersion}
-🔄 *Update:* ${resl.info.latestUpdate}
-⭐️ *Rating:* ${resl.info.contentRating}
-📋 *Requirements:* ${resl.info.requirements}
-📏 *Size:* ${resl.download.size}
-📚 *Guide:* ${resl.download.guide}
-🔗 *Link Full:* ${encodeURI(resl.download.linkFull)}
+                let res = await getHippo(inputs)
+                let teks = res.map((item, index) => {
+                    return `🔍 *[ RESULT ${index + 1} ]*
 
-${wait}`
-                await conn.sendFile(m.chat, resl.info.ogImageUrl, "", cap, m)
-                await conn.sendFile(m.chat, resl.download.linkMod, resl.info.title, null, m, true, {
+📌 *title:* ${item.name}
+🖼️ *image:* ${item.icon_url}
+🔗 *url:* ${item.html_url}
+⬇️ *downloadLink:* ${item.download_url}
+📦 *app:* ${item.app_name}
+📱 *deviceInfo:* ${item.information}
+🤖 *version:* ${item.version}
+📋 *mime:* ${item.mimetype}
+`
+
+                }).filter(v => v).join("\n\n________________________\n\n")
+                await conn.sendFile(m.chat, res[0].icon_url || logo, "", teks, m)
+                await conn.sendFile(m.chat, res[0].download_url || logo, res[0].name || 'Tidak diketahui', null, m, true, {
                     quoted: m,
-                    mimetype: "application/vnd.android.package-archive"
+                    mimetype: res[0].mimetype
                 })
             } catch (e) {
                 await m.reply(eror)
             }
         }
+
     }
 }
-handler.help = ["apk4all"]
+handler.help = ["filehippo"]
 handler.tags = ["internet"]
-handler.command = /^(apk4)$/i
+handler.command = /^(apps)$/i
 export default handler
 
 /* New Line */
-async function searchApk4all(query) {
-  const url = `https://apk4all.io/search/${query}`; // Ganti dengan URL yang sesuai
+async function searchHippo(app_name) {
+    const url = 'https://filehippo.com/search/?q=' + app_name; // Ganti dengan URL halaman yang ingin Anda scraping
 
-  try {
-    const response = await fetch(url);
-    const data = await response.text();
-    const $ = cheerio.load(data);
-    const articles = [];
+    try {
+        const response = await fetch(url);
+        const html = await response.text();
+        const $ = cheerio.load(html);
+        const programs = await Promise.all(
+            $('.list-programs__item').map(async (index, element) => {
+                const title = $('.card-program__title', element).text().trim();
+                const link = $('.card-program', element).attr('href');
+                const image = $('.media__image img', element).attr('src');
+                const programTitle = $('.card-program__title', element).text().trim();
+                const programLink = $('.card-program', element).attr('href');
+                const developer = $('.card-program__developer', element).text().trim();
+                const license = $('.card-program__license', element).text().trim();
+                const summary = $('.card-program__summary', element).text().trim();
 
-    $('article').each((index, element) => {
-      const $article = $(element);
-      const title = $article.find('.entry-title a').text().trim();
-      const titleUrl = $article.find('.entry-title a').attr('href');
-      const imageUrl = $article.find('.apk-dl .icon img').attr('src');
-      const rating = $article.find('.details-rating .average.rating').text().trim();
-      const views = $article.find('.details-rating .details-delimiter').eq(1).text().replace(/\n|\|\s|\t/g, '').trim();
-      const average = $article.find('.details-rating .stars').attr('title').trim();
-      const updateDate = $article.find('.details-rating .update-date').next().text().trim();
+                if (title && link && image && programTitle && programLink && developer && license && summary) {
+                    return {
+                        title,
+                        link,
+                        image,
+                        programTitle,
+                        programLink,
+                        developer,
+                        license,
+                        summary
+                    };
+                }
+            }).get()
+        );
 
-      articles.push({ title, titleUrl, imageUrl, rating, average, views, updateDate });
-    });
-
-    return articles;
-  } catch (error) {
-    throw new Error('Error fetching data:', error);
-  }
+        return programs.filter(Boolean);
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
-async function getApk4all(url) {
-  try {
-    const response = await fetch(url);
-    const html = await response.text();
+async function getHippo(app_name) {
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'
+    };
+    const urls = {
+        information: (app_name.endsWith('/') ? app_name : app_name + '/'),
+        download: (app_name.endsWith('/') ? app_name : app_name + '/') + 'post_download/'
+    };
+
+    const [html, downloadHtml] = await Promise.all(Object.values(urls).map(url => fetch(url, {
+        headers
+    }).then(response => response.text())));
+
     const $ = cheerio.load(html);
-    const info = {
-      title: $('.dllinks .da').attr('title'),
-      link: $('.dllinks .da').attr('href'),
-      ogImageUrl: $('meta[property="og:image"]').attr('content'),
-      developer: $('td:contains("Developer:")').next().text().trim(),
-      currentVersion: $('td:contains("Current Version:")').next().text().trim(),
-      latestUpdate: $('td:contains("Latest Update:")').next().find('time').text().trim(),
-      contentRating: $('td:contains("Content Rating:")').next().text().trim(),
-      getItOn: $('td:contains("Get it on:")').next().find('a').attr('href'),
-      requirements: $('td:contains("Requirements:")').next().find('a').text().trim(),
-      appID: $('td:contains("App ID:")').next().text().trim()
-    };
+    const hippo_information_name = $('body > div.page > div:nth-child(2) > div > div > div > section.program-header-content > div.program-header-content__main > div > div.media__body > h1').text();
+    const hippo_information = $('body > div.page > div:nth-child(2) > div > div > div > section.mb-l > article > p:nth-child(3)').text();
+    const hippo_version = $('body > div.page > div:nth-child(2) > div > div > div > section.program-header-content > div.program-header-content__main > div > div.media__body > p.program-header__version').text();
+    const hippo_icon_url = $('body > div.page > div:nth-child(2) > div > div > div > section.program-header-content > div.program-header-content__main > div > div.media__image > img').attr('src');
+    const hippo_download_url = cheerio.load(downloadHtml)('script[data-qa-download-url]').attr('data-qa-download-url');
 
-    const response2 = await fetch(info.link);
-    const html2 = await response2.text();
-    const $two = cheerio.load(html2);
+    const mimetype = await getMimeTypeFromUrl(hippo_download_url);
 
-    const download = {
-      title: $two('.box h1.title').text().trim(),
-      linkFull: $two('.box p.field a.button.is-danger').attr('href'),
-      linkMod: $two('.box div.buttons div.field p.control a.button.is-primary').attr('href'),
-      size: $two('.box div.field.has-addons p.control.is-expanded a.button.is-primary').text().trim(),
-      qr: $two('.box div.field.has-addons p.control a.zb.button.is-primary img.qr').attr('src'),
-      guide: $two('.box div.block.content.notification.is-info.is-light.container').text().trim()
-    };
+    return [{
+        name: hippo_information_name,
+        version: hippo_version,
+        information: hippo_information,
+        html_url: urls.information,
+        download_url: hippo_download_url,
+        icon_url: hippo_icon_url,
+        app_name,
+        mimetype,
+    }];
+}
 
-    return { info, download };
-  } catch (error) {
-    throw new Error('Error fetching additional information:', error);
-  }
-};
+async function getMimeTypeFromUrl(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const contentType = response.headers.get('content-type');
+            return contentType;
+        } else {
+            throw new Error('Failed to fetch URL');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
