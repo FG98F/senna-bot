1d7c900ea0ad29edbd240767fafb033ae0657f54
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 13:54:31 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -5,9 +5,9 @@ let name = conn.getName(m.sender)
 let av = `./src/mp3/${pickRandom(["criss", "andrea"])}.mp3`
 
       conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-      ['⦙☰ Menu', '/help'],
+      ['⦙☰ Menu', '/menu'],
       ['⦙☰ Menu 2', '/menu2'],
-      [`⌬ ${mssg.gp}s`, '/gpdylux']
+      [`⌬ ${mssg.gp}s`, '/support']
     ], m) 
     conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
