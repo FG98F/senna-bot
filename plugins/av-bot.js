dae110707299bbb5a105e49775dd5c987c8d387f
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-19 16:38:16 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -3,24 +3,19 @@ let handler = async (m, { conn }) => {
   let name = await conn.getName(m.sender);
   let av = `./src/fg_logo.jpg`;
   let pp = `./src/fg_logo.jpg`;
-
   let buttons = [
-    [{ text: "⦙☰ Menu", callbackData: "/menu" }],
-    [{ text: "⦙☰ Menu 2", callbackData: "/menu2" }],
-    [{ text: `⌬ ${mssg.gp}s`, callbackData: "/support" }]
+    { buttonId: "/menu", buttonText: { displayText: "⦙☰ Menu" }, type: 1 },
+    { buttonId: "/menu2", buttonText: { displayText: "⦙☰ Menu 2" }, type: 1 },
+    { buttonId: "/support", buttonText: { displayText: `⌬ ${mssg.gp}s` }, type: 1 }
   ];
-
   let message = `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`;
-
   await conn.sendFile(m.chat, av, message, mssg.ig, null, m, rpyt, true, { type: "audioMessage", ptt: true });
   await conn.sendMessage(m.chat, { image: { url: pp }, buttons: buttons }, { quoted: m });
-
   m.react("📃");
 };
 
 handler.customPrefix = /^(bot|senna)$/i;
 handler.command = new RegExp();
-
 export default handler;
 
 function pickRandom(list) {
