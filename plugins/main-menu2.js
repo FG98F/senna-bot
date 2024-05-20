571c9662f4072da4746dd0b2bca177349f66d565
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-20 10:45:26 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,35 +1,25 @@
+
+
 let handler = async function (m, { conn, text, usedPrefix }) {
   
-  let m2 = `
-  ◈ ━━━━ *𝑴𝒊𝒓𝒛𝒂  ┃ ᴮᴼᵀ* ━━━━━ ◈ *${usedPrefix}*
-   👋🏻 مرحباً! : %name
-   👥 عدد المستخدمين : %totalreg
-   🟢 وقت النشاط : %muptime%sbot
-  ▢ 
-  └──────────────
-  `
-  let pp = './src/fg_logo.jpg'
-  let buttons = [
-    [ '⦙☰ قائمة الاوامر', `${usedPrefix}اوامر`],
-    [ '⌬ جروب الدعم', `${usedPrefix}support`]
-  ]
-  let buttonMessage = {
-    image: pp,
-    caption: m2,
-    buttons: buttons,
-    headerType: 4
-  }
-  conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-    .then(() => {
-      // تأكد من أن البوت لديه الصلاحيات لإضافة التفاعلات
-      m.react('📃')
-        .catch(console.error) // في حال وجود خطأ سيتم طباعته
-    })
-    .catch(console.error) // في حال وجود خطأ في إرسال الرسالة سيتم طباعته
+let m2 = `
+◈ ━━━━ *𝑴𝒊𝒓𝒛𝒂  ┃ ᴮᴼᵀ* ━━━━━ ◈ *${usedPrefix}*
+ 👋🏻 مرحباً! : %name
+ 👥 عدد المستخدمين : %totalreg
+ 🟢 وقت النشاط : %muptime%sbot
+▢ 
+└──────────────
+`
+     let pp =  ./src/fg_logo.jpg  
+      conn.sendButton2(m.chat, m2, mssg.ig, pp, [
+      [ ⦙☰ قائمة الاوامر , `${usedPrefix}اوامر`],
+      [ ⌬ جروب الدعم , `${usedPrefix}support`]
+    ],m, rpyt) 
+ 
 }
 
-handler.help = ['help']
-handler.tags = ['main']
-handler.command = ['menu', 'اوامر']
+handler.help = [ help ]
+handler.tags = [ main ]
+handler.command = [ menu , اوامر ] 
 
 export default handler
