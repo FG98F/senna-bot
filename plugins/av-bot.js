ec5200f03d8abeeb1e22277772222121e11f4897
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 16:40:16 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,16 +1,17 @@
 
-let handler = async (m, { conn}) => {
-
-let name = conn.getName(m.sender)
-let av = `./src/fg_logo.jpg`
+let handler = async (m, { conn }) => {
+  let name = conn.getName(m.sender)
+  let av = `./src/fg_logo.jpg`
 
   conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-      ['⦙☰ Menu', '/menu'],
-      ['⦙☰ Menu 2', '/menu2'],
-      [`⌬ ${mssg.gp}s`, '/support']
-    ], m) 
-conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-} 
+    [⦙☰ Menu,  /menu ],
+    [⦙☰ Menu 2,  /menu2 ],
+    [`⌬ ${mssg.gp}s`,  /support ]
+  ], m)
+
+  // إضافة إرسال الصورة هنا
+  conn.sendFile(m.chat, av,  audio.mp3 , null, m, true, { type:  audioMessage , ptt: true })
+}
 
 handler.customPrefix = /^(bot|senna)$/i
 handler.command = new RegExp
