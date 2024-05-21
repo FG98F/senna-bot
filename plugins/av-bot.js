6a6b6a57989f3edaf94c5a0f029f84fff6b6e28b
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 19:55:41 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
   ], m) // هذا هو وصف الصورة الذي سيحتوي على القائمة
 
   // إرسال الصورة مع الوصف الذي يحتوي على القائمة
-  
+  conn.sendFile(m.chat, './src/fg_logo.jpg'  , 'fg_logo.jpg'  , imageCaption, m)
 }
 
 handler.customPrefix = /^(bot|senna)$/i
