41583184d2482e7fa2dd561dabbe462a5409ef19
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 19:53:36 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,6 +1,5 @@
 let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
-  let image = conn.sendFile(m.chat, './src/fg_logo.jpg'  , 'fg_logo.jpg' , imageCaption, m)
   let imageCaption = conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
     ['⦙☰ Menu' , '/menu' ],
     ['⦙☰ Menu 2' , '/menu2' ],
