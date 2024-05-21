497b017c878482ff18b36ecf3658819bf7659472
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 17:41:09 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -2,20 +2,20 @@ let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
   let imageCaption = `${mssg.hi} *${name}* \n\n${mssg.bohelp}` // هذا هو وصف الصورة الذي سيحتوي على القائمة
 
-  // إرسال الصورة مع الوصف الذي يحتوي على القائمة
-  conn.sendFile(m.chat, './src/fg_logo.jpg', 'fg_logo.jpg', imageCaption, m)
-  conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-    ['⦙☰ Menu', '/menu'],
-    ['⦙☰ Menu 2', '/menu2'],
-    [`⌬ ${mssg.gp}s`, '/support']
-  ], m)
+  // إرسال الصورة مع القائمة كرسالة تفاعلية
+  let message = {
+    image: { url:  './src/fg_logo.jpg'  },
+    caption: imageCaption,
+    buttons: [
+      { buttonId:  '/menu' , buttonText: { displayText:  '⦙☰ Menu'  }, type: 1 },
+      { buttonId:  '/menu2' , buttonText: { displayText:  '⦙☰ Menu 2'  }, type: 1 },
+      { buttonId:  '/support' , buttonText: { displayText:  '⌬ Support'  }, type: 1 }
+    ]
+  }
+  await conn.sendMessage(m.chat, message, { quoted: m })
 }
 
 handler.customPrefix = /^(bot|senna)$/i
 handler.command = new RegExp
 
 export default handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
