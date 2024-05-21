4e88b2904179565ddbc894607a62d229d42c86bc
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 19:01:01 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,15 +1,14 @@
 let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
-  let imageCaption = `${mssg.hi} *${name}* \n\n${mssg.bohelp}` // هذا هو وصف الصورة الذي سيحتوي على القائمة
-
-  // إرسال الصورة مع الوصف الذي يحتوي على القائمة
-  conn.sendFile(m.chat,  './src/fg_logo.jpg' ,  'fg_logo.jpg' , imageCaption, m)
-  conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+  let imageCaption = 'conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
     ['⦙☰ Menu', '/menu' ],
     ['⦙☰ Menu 2', '/menu2' ],
     [`⌬ ${mssg.gp}s`, '/support' ]
   ], m)
-}
+}' // هذا هو وصف الصورة الذي سيحتوي على القائمة
+
+  // إرسال الصورة مع الوصف الذي يحتوي على القائمة
+  conn.sendFile(m.chat,  './src/fg_logo.jpg' ,  'fg_logo.jpg' , imageCaption, m)
 
 handler.customPrefix = /^(bot|senna)$/i
 handler.command = new RegExp
