3ee4b5bd9ac6c3111513f737d478558c93243fe2
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 19:03:16 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
-  let imageCaption = 'conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+  let imageCaption = conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
     ['⦙☰ Menu', '/menu' ],
     ['⦙☰ Menu 2', '/menu2' ],
     [`⌬ ${mssg.gp}s`, '/support' ]
   ], m)
-}' // هذا هو وصف الصورة الذي سيحتوي على القائمة
+} // هذا هو وصف الصورة الذي سيحتوي على القائمة
 
   // إرسال الصورة مع الوصف الذي يحتوي على القائمة
   conn.sendFile(m.chat,  './src/fg_logo.jpg' ,  'fg_logo.jpg' , imageCaption, m)
