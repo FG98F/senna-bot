02559a568e1376b664774f895f8f85ef00088110
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 16:59:57 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,16 +1,15 @@
-
 let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
   let av = `./src/fg_logo.jpg`
 
-  conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-    ['⦙☰ Menu', '/menu' ],
-    ['⦙☰ Menu 2', '/menu2' ],
-    [`⌬ ${mssg.gp}s`, '/support' ]
-  ], m)
+  // إضافة إرسال الصورة هنا قبل إرسال القائمة
+  conn.sendFile(m.chat, av, 'fg_logo.jpg', null, m, true, { type: 'audioMessage', ptt: true })
 
-  // إضافة إرسال الصورة هنا
-  conn.sendFile(m.chat, av, 'fg_logo.jpg' , null, m, true, { type:  'audioMessage' , ptt: true })
+  conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+    ['⦙☰ Menu', '/menu'],
+    ['⦙☰ Menu 2', '/menu2'],
+    [`⌬ ${mssg.gp}s`, '/support']
+  ], m)
 }
 
 handler.customPrefix = /^(bot|senna)$/i
