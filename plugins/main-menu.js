1b8205a42825203bf043f26bb46e43f9956a1fa7
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 14:41:35 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -145,7 +145,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       conn.sendButton2(m.chat, text.trim(), `▢ 𝑴𝒊𝒓𝒛𝒂 ┃ ᴮᴼᵀ\n${mssg.ig}`, pp, [
       ['⏍ معلومات البوت', `${_p}botinfo`],
       ['⌬ الدعم', `${_p}support`]
-    ], m, rpl, rcanal)
+    ], m, rpl)
     conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rcanal)
   
     m.react('📚') 
