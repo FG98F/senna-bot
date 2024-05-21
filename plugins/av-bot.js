9ef951df05f1a921f45e7ecbfd7f13d1bfac2f1c
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-21 19:10:32 +0300

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,14 +1,14 @@
 let handler = async (m, { conn }) => {
   let name = conn.getName(m.sender)
-  let imageCaption = conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
-    ['⦙☰ Menu', '/menu' ],
-    ['⦙☰ Menu 2', '/menu2' ],
-    [`⌬ ${mssg.gp}s`, '/support' ]
-  ], m)
+  let imageCaption = conn.sendFile(m.chat,  './src/fg_logo.jpg' ,  'fg_logo.jpg' , imageCaption, m)
   // هذا هو وصف الصورة الذي سيحتوي على القائمة
 
   // إرسال الصورة مع الوصف الذي يحتوي على القائمة
-  conn.sendFile(m.chat,  './src/fg_logo.jpg' ,  'fg_logo.jpg' , imageCaption, m)
+  conn.sendButton2(m.chat, `${mssg.hi} *${name}* \n\n${mssg.bohelp} \n`, mssg.ig, null, [
+    [ ⦙☰ Menu ,  /menu  ],
+    [ ⦙☰ Menu 2 ,  /menu2  ],
+    [`⌬ ${mssg.gp}s`,  /support  ]
+  ], m)
 }
 
 handler.customPrefix = /^(bot|senna)$/i
