dbeea5111ba1f4fc78d9919fbccc872e11892df4
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-22 17:11:13 +0300

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,23 +1,23 @@
-import { search, download } from  aptoide-scraper ;
+import { search, download } from 'aptoide-scraper';
 
 const handler = async (m, { conn, usedPrefix, command, text }) => {
-  if (!text) return conn.reply(m.chat,  '*تحميل التطبيقات محبوب الجماهير يقوم بتحميل التطبيقات*\n\n*مثــال:*\n .apk facebook lite' , m);
+  if (!text) return conn.reply(m.chat, '*تحميل التطبيقات محبوب الجماهير يقوم بتحميل التطبيقات*\n\n*مثــال:*\n .apk facebook lite', m);
 
   try {
     let searchResults = await search(text);
     if (searchResults.length === 0) {
-      return conn.reply(m.chat,   '*لا توجد نتائج لهذا التطبيق*' , m);
+      return conn.reply(m.chat, ' *لا توجد نتائج لهذا التطبيق*', m);
     }
 
     let data = await download(searchResults[0].id);
     if (!data || !data.name || !data.package || !data.lastup || !data.size || !data.dllink || !data.icon) {
-      return conn.reply(m.chat,  '*لا يمكن الحصول على تفاصيل التطبيق*' , m);
+      return conn.reply(m.chat, '*لا يمكن الحصول على تفاصيل التطبيق*', m);
     }
 
     let response = `💌 *اسم التطبيق:* ${data.name}\n📦 *الحزمة:* ${data.package}\n🕒 *آخر تحديث:* ${data.lastup}\n📥 *الحجم:* ${data.size}\n\n_إنضم لعائلة JITOSSA عبر الضغط على الرابط_\n`;
 
-    if (data.size.includes( GB ) || parseFloat(data.size.replace(  MB ,   )) > 999) {
-      return conn.reply(m.chat,  '🚩 *الملف ثقيل جدًا*' , m);
+    if (data.size.includes('GB') || parseFloat(data.size.replace(' MB', '')) > 999) {
+      return conn.reply(m.chat, '🚩 *الملف ثقيل جدًا*', m);
     }
 
     const iconUrl = data.icon; // رابط أيقونة التطبيق
@@ -26,8 +26,8 @@ const handler = async (m, { conn, usedPrefix, command, text }) => {
       contextInfo: {
         externalAdReply: {
           title: data.name,
-          body:  'إضغط للمتابعة على إنستجرام' ,
-          sourceUrl:  'https://www.instagram.com/ovmar_1?igsh=bHFxbnl5ZmVyaGN0' ,
+          body: 'إضغط للمتابعة على إنستجرام',
+          sourceUrl: 'https://www.instagram.com/ovmar_1?igsh=bHFxbnl5ZmVyaGN0',
           thumbnailUrl: iconUrl, // icon application 
           mediaType: 1, // matbdl walo
           showAdAttribution: true,
@@ -36,15 +36,15 @@ const handler = async (m, { conn, usedPrefix, command, text }) => {
       }
     }, { quoted: m });
 
-    await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype:  application/vnd.android.package-archive , fileName: data.name +  .apk , caption: null }, { quoted: m });
+    await conn.sendMessage(m.chat, { document: { url: data.dllink }, mimetype: 'application/vnd.android.package-archive', fileName: data.name + '.apk', caption: null }, { quoted: m });
   } catch (error) {
     console.error(error);
-    return conn.reply(m.chat,  '*حدث خطأ أثناء معالجة الطلب*' , m);
+    return conn.reply(m.chat, '*حدث خطأ أثناء معالجة الطلب*', m);
   }
 };
 
-handler.tags = [ 'applications' ];
-handler.help = [ 'apk' ];
-handler.command = /^(apk|apkdl|dapk2|تطبيق|aptoidedl)$/i;
+handler.tags = ['applications'];
+handler.help = ['apk'];
+handler.command = /^(apk|apkdl|dapk2|aptoide|aptoidedl)$/i;
 
 export default handler;
