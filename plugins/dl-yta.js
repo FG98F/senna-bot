b8f8b2867d76041cddba9092a568820efc5a19cd
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-22 15:18:20 +0300

    Update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -1,42 +1,42 @@
 
-import ytdl from  ytdl-core 
-import fs from  fs 
-import { pipeline } from  stream 
-import { promisify } from  util 
-import os from  os 
-import fg from  api-dylux 
-import fetch from  node-fetch 
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+import os from 'os'
+import fg from 'api-dylux'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   if (!args || !args[0]) throw `✳️ ${mssg.example} :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
-  if (!args[0].match(/youtu/gi)) throw `❎ ${mssg.noLink( YouTube )}`
+  if (!args[0].match(/youtu/gi)) throw `❎ ${mssg.noLink('YouTube')}`
    m.react(rwait)
  let chat = global.db.data.chats[m.chat]
- let q =  128kbps 
+ let q = '128kbps'
  
  try {
  
-		//let res = await fetch(global.API( fgmods ,  /api/downloader/yta , { url: args[0], quality: q},  apikey ))
+		//let res = await fetch(global.API('fgmods', '/api/downloader/yta', { url: args[0], quality: q}, 'apikey'))
 		//let yt = await res.json()
 		const yt = await fg.yta(args[0]) 
 		let { title, dl_url, quality, size, sizeB } = yt
 		
-		conn.sendFile(m.chat, dl_url, title +  .mp3 , `
+		conn.sendFile(m.chat, dl_url, title + '.mp3', `
  ≡  *FG YTDL*
   
 ▢ *📌${mssg.title}* : ${title}
 ▢ *⚖️${mssg.size}* : ${size}
-`.trim(), m, false, { mimetype:  audio/mpeg , asDocument: chat.useDocument })
+`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done)
  	} catch {
   try {
   	
 	const { title, dl_url } = await ytmp3(args[0]);
   
-		conn.sendFile(m.chat, dl_url, title +  .mp3 , `
+		conn.sendFile(m.chat, dl_url, title + '.mp3', `
  ≡  *FG YTDL 2*
   
 ▢ *📌${mssg.title}* : ${title}
-`.trim(), m, false, { mimetype:  audio/mpeg , asDocument: chat.useDocument })
+`.trim(), m, false, { mimetype: 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done)
         } catch {
 			await m.reply(`❎ ${mssg.error}`)
@@ -44,9 +44,9 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 }
 
 }
-handler.help = [ 'ytmp3 <url>' ]
-handler.tags = [ 'dl' ]
-handler.command = [ 'ytmp3' ,  'fgmp3' ] 
+handler.help = ['ytmp3 <url>']
+handler.tags = ['dl']
+handler.command = ['ytmp3', 'fgmp3'] 
 handler.diamond = false
 
 export default handler
@@ -59,7 +59,7 @@ async function ytmp3(url) {
     const { title, thumbnails, lengthSeconds, viewCount, uploadDate } = videoDetails;
     const thumbnail = thumbnails[0].url;
     
-    const audioStream = ytdl(url, { filter:  audioonly , quality:  highestaudio  });
+    const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
     const tmpDir = os.tmpdir();
     const audioFilePath = `${tmpDir}/${title}.mp3`;
 
@@ -70,7 +70,7 @@ async function ytmp3(url) {
         views: viewCount,
         publish: uploadDate,
         duration: lengthSeconds,
-        quality:  128kbps ,
+        quality: '128kbps',
         thumb: thumbnail,
         dl_url: audioFilePath
     };
