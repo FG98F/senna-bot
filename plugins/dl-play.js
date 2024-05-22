fc6e061ddebbd4375a78725d3095d6776a93de05
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-22 16:07:31 +0300

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 ▢ 👀 *المشاهدات:* ${vid.views.toLocaleString()}
 └──────────────`
  await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
-    ['🎶 صوت', `${usedPrefix}fgmp3 ${url}`],
+    ['🎶 صوت', `${usedPrefix}song ${url}`],
     ['🎥 فيديو', `${usedPrefix}fgmp4 ${url}`]
   ], null, [['قناة البوت', `${fgcanal}`]], m)
 }
