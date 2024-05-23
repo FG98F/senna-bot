52f4762c9cecfa19cfbfd983beae43ae46b86822
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-23 16:21:28 +0300

    Update JOHAN-احزر.js

--- plugins/JOHAN-احزر.js
+++ plugins/JOHAN-احزر.js
@@ -1,11 +1,11 @@
-import similarity from  similarity ;
+import similarity from 'similarity';
 const threshold = 0.72;
 const handler = (m) => m;
 handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply( لقد انتهى هذا سؤال الآن! );
+  if (!(id in this.tekateki)) return m.reply('لقد انتهى هذا سؤال الآن!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
@@ -14,7 +14,7 @@ handler.before = async function(m) {
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
     } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`اقتربت من الاجابة!`);
-    else m.reply( إجابة خاطئة! );
+    else m.reply('إجابة خاطئة!');
   }
   return !0;
 };
