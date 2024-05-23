4bfcee9d24b70cd90771291252cc4d230f87f92d
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-23 13:06:36 +0300

    Update play3.js

--- plugins/play3.js
+++ plugins/play3.js
@@ -1,24 +1,70 @@
-import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'هذا الامر خاص بتحميل الفيديوات على شكل mp3 من اليوتوب '
-await m.reply( 'جاري تحميل mp3 المرجو الانتظار قليلا رجاء في حالة لم يقم البوت بتحميل mp3 الخاص بك  اعلم ان الفيديو طويل او ان حجمه كبير  ويمكنك مراسلة المطور للمزيد من المعلومات https://solo.to/mr_sasa')
-try {
-let q = '128kbps'
-let v = args[0]
-const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
-const dl_url = await yt.audio[q].download()
-const ttl = await yt.title
-const size = await yt.audio[q].fileSizeH
-await conn.sendFile(m.chat, dl_url, ttl + '.mp3', null, m, false, { mimetype: 'audio/mp4' })
-} catch {
-try {
-let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
-let lolh = await lolhuman.json()
-let n = lolh.result.title || 'error'
-await conn.sendFile(m.chat, lolh.result.link, `${n}.mp3`, null, m, false, { mimetype: 'audio/mp4' })    
-} catch {
-await conn.reply(m.chat, '*[❀] 𝙴𝚁𝚁𝙾𝚁*', m)}
-}}
-handler.command = /^اغنيه$/i
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+import yts from 'yt-search'
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `*تحميل الاغاني من يوتوب*`
+  await m.reply(wait)
+  let search = await yts(text)
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
+  if (!search) throw '*⚠️ Vídeo no encontrado, prueba con otro título*'
+  let { title, thumbnail, timestamp, views, ago, url } = vid
+
+  let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ T: ${title}
+⬡ D: ${timestamp}
+⬡ V: ${views}
+⬡ S: ${ago}
+⬡ E: ${url}
+╰────────⬣`
+  conn.sendButton(m.chat, `╭──── 〔 Y O U T U B E 〕 ─⬣
+⬡ T: ${title}
+⬡ D: ${timestamp}
+⬡ V: ${views}
+⬡ S: ${ago}
+⬡ E: ${url}
+╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['📽 VIDEO', `${usedPrefix}getvid ${url} 360`], false, { quoted: m, 'document': { 'url':'https://wa.me/59176184204' },
+'mimetype': global.dpdf,
+'fileName': `𝕐𝕠𝕦𝕋𝕦𝕓𝕖 ℙ𝕝𝕒𝕪`,
+'fileLength': 666666666666666,
+'pageCount': 2023,contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: `${url}`,
+title: `BOBIZ IS HERE ♥...`,
+body:author,
+sourceUrl: 'http://wa.me/59176184204', thumbnail: await ( await conn.getFile(thumbnail)).data
+  }
+ } 
+})
+  
+  //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
+ //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+
+  const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
+const link = await yt.audio['128kbps'].download()
+  let doc = { 
+  audio: 
+  { 
+    url: link 
+}, 
+mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
+mediaType:  2,
+mediaUrl: url,
+title: title,
+body: author,
+sourceUrl: url,
+thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
+                                                                                                                 }
+                       }
+  }
+
+  return conn.sendMessage(m.chat, doc, { quoted: m })
+	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
+	// return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
+}
+handler.help = ['play'].map(v => v + ' <pencarian>')
+handler.tags = ['downloader']
+handler.command = /^اغاني$/i
+
+handler.exp = 0
+handler.limit = false
+
 export default handler
