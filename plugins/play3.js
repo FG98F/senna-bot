0051815eb304a80531deb085ffdac0c22067da47
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-23 12:40:10 +0300

    Update play3.js

--- plugins/play3.js
+++ plugins/play3.js
@@ -1,36 +1,76 @@
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
+import ytdl from 'ytdl-core';
+import yts from 'yt-search';
+import fs from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import os from 'os';
 
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `✳️ Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`;
-  if (!args[0].match(/youtu/gi)) throw `❎ Verify that it is a YouTube link.`;
+const streamPipeline = promisify(pipeline);
 
-  m.react(rwait); 
+var handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `مثال : \n ${usedPrefix}${command} midle of night`;
 
-  try {
-    let q = '128kbps'; 
-    let v = args[0]; 
-    const yt = await youtubedl(v).catch(async () => await youtubedlv2(v)); 
-    const dl_url = await yt.audio[q].download(); 
-    const title = await yt.title; 
+  let search = await yts(text);
+  let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
+  if (!search) throw 'الفديو غير موجود, جرب عنوان ثاني';
+  let { title, thumbnail, timestamp, views, ago, url } = vid;
+  let wm = ' 💝AHMAD SHİNİCHİ💝';
 
-    conn.sendFile(
-      m.chat,
-      dl_url,
-      title + '.mp3',
-      null, 
-      m,
-      false,
-      { mimetype: 'audio/mpeg' }
-    );
+  let captvid = `💝  جاري التحميل ♥`;
 
-    m.react(done); 
-  } catch {
-    await m.reply(`❎ Error: Could not download the audio.`)
-  }
+  conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
+
+
+  const audioStream = ytdl(url, {
+    filter: 'audioonly',
+    quality: 'highestaudio',
+  });
+
+  // Get the path to the system's temporary directory
+  const tmpDir = os.tmpdir();
+
+  // Create writable stream in the temporary directory
+  const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
+
+  // Start the download
+  await streamPipeline(audioStream, writableStream);
+
+  let doc = {
+    audio: {
+      url: `${tmpDir}/${title}.mp3`
+    },
+    mimetype: 'audio/mp4',
+    fileName: `${title}`,
+    contextInfo: {
+      externalAdReply: {
+        showAdAttribution: true,
+        mediaType: 2,
+        mediaUrl: url,
+        title: title,
+        body: wm,
+        sourceUrl: url,
+        thumbnail: await (await conn.getFile(thumbnail)).data
+      }
+    }
+  };
+
+  await conn.sendMessage(m.chat, doc, { quoted: m });
+
+  // Delete the audio file
+  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+    if (err) {
+      console.error(`Failed to delete audio file: ${err}`);
+    } else {
+      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+    }
+  });
 };
 
-handler.help = ['ytmp3 <url>']
-handler.tags = ['downloader']
-handler.command = ['ytmp5', 'yta'] 
+handler.help = ['play'].map((v) => v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = ['mp3', 'songs', 'اغنيه']
 
-export default handler
+handler.exp = 0;
+handler.diamond = false;
+
+export default handler;
