1e1938b95553542e96e05e848ed5e1e93cb4de5a
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-24 06:20:51 +0300

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -14,9 +14,9 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
   let vid = search.videos[Math.floor(Math.random() * search.videos.length)];
   if (!search) throw 'Video Not Found, Try Another Title';
   let { title, thumbnail, timestamp, views, ago, url } = vid;
-  let wm = ' *JOHAN* '; //حط اسم بوتك
+  let wm = ' 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 '; //حط اسم بوتك
 
-  let captvid = `  جاري التحميل `;
+;
 
   conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author }, { quoted: m });
 
