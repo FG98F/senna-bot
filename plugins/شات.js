3ca14ea21352f06942757fc9f472ab92da76128c
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-25 16:08:42 +0300

    Create شات.js

---
+++ plugins/شات.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `تقصد نفسك`;
+    await conn.sendMessage(m.chat, {
+   react: {
+ text: "💀",
+ key: m.key,
+   }
+  })
+
+    conn.sendFile(m.chat, 'https://telegra.ph/file/caafdb2292db8180b99fe.mp4', 'video.mp4' , message, m);
+};
+
+handler.customPrefix = /^(bot|بوت)$/i;
+handler.command = new RegExp;
+
+export default handler;
