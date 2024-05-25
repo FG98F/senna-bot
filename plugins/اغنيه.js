cbcab4e92537a23f815b379bfc77de28df1ace88
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-26 00:08:55 +0300

    Update اغنيه.js

--- plugins/اغنيه.js
+++ plugins/اغنيه.js
@@ -9,47 +9,49 @@ const streamPipeline = promisify(pipeline);
 var handler = async (m, { conn, command, text, usedPrefix }) => {
   if (!text) throw `مثال : \n ${usedPrefix}${command} midle of night`;
   const url = text.trim();
-
   let vidInfo = await ytdl.getInfo(url);
   let { videoDetails: { title, thumbnails, lengthSeconds } } = vidInfo;
   let thumbnail = thumbnails[thumbnails.length - 1].url;
-
   let wm = '♪ 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 ♪'; // Your bot's watermark
-          m.react(rwait)
-  
+  m.react(rwait);
 
   const audioStream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
   const tmpDir = os.tmpdir();
   const writableStream = fs.createWriteStream(`${tmpDir}/${title}.mp3`);
-  await streamPipeline(audioStream, writableStream);
 
-  let doc = {
-    audio: {
-      url: `${tmpDir}/${title}.mp3`
-    },
-    mimetype: 'audio/mp4',
-    fileName: title,
-    contextInfo: {
-      externalAdReply: {
-        showAdAttribution: true,
-        mediaType: 2,
-        mediaUrl: url,
-        title: title,
-        body: wm,
-        sourceUrl: url,
-        thumbnail: await (await conn.getFile(thumbnail)).data
+  try {
+    await streamPipeline(audioStream, writableStream);
+    let doc = {
+      audio: {
+        url: `${tmpDir}/${title}.mp3`
+      },
+      mimetype: 'audio/mp4',
+      fileName: title,
+      contextInfo: {
+        externalAdReply: {
+          showAdAttribution: true,
+          mediaType: 2,
+          mediaUrl: url,
+          title: title,
+          body: wm,
+          sourceUrl: url,
+          thumbnail: await (await conn.getFile(thumbnail)).data
+        }
       }
-    }
-  };
-  await conn.sendMessage(m.chat, doc, { quoted: m });
-       m.react(done)
-  fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
-    if (err) {
-      console.error(`Failed to delete audio file: ${err}`);
-    } else {
-      console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
-    }
-  });
+    };
+    await conn.sendMessage(m.chat, doc, { quoted: m });
+    m.react(done);
+    fs.unlink(`${tmpDir}/${title}.mp3`, (err) => {
+      if (err) {
+        console.error(`Failed to delete audio file: ${err}`);
+      } else {
+        console.log(`Deleted audio file: ${tmpDir}/${title}.mp3`);
+      }
+    });
+  } catch (error) {
+    console.error(`Error processing audio file: ${error}`);
+    m.reply('An error occurred while processing the audio file');
+  }
 };
 
 handler.help = ['<query>'];
