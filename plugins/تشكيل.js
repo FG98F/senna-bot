e3c27ac5410d28fea5e4e46639a5af8a96cdb23d
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-25 18:52:34 +0300

    Create تشكيل.js

---
+++ plugins/تشكيل.js
@@ -0,0 +1,22 @@
+let handler = async(m, { conn, text , command }) => {
+if (command == 'تشكيل') {
+if (!text) throw '*مثال* :\n*.تشكيل* ♥♥';
+let awikwok = `👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿
+👉🏿👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👈🏿
+👉🏿👉🏾👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👇🏼👇🏼👇🏼👇🏼👇🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👇🏻👇🏻👇🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👉🏻${text}👈🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👉🏼👆🏻👆🏻👆🏻👈🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👉🏽👆🏼👆🏼👆🏼👆🏼👆🏼👈🏽👈🏾👈🏿
+👉🏿👉🏾👆🏽👆🏽👆🏽👆🏽👆🏽👆🏽👈🏽👈🏾👈🏿
+👉🏿👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👈🏿
+👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿`
+m.reply(awikwok, null, m.mentionedJid ? {
+  mentions: m.mentionedJid
+} : {})
+}
+}
+handler.command = handler.help = ['تشكيل']
+handler.tags = ['fun']
+export default handler
