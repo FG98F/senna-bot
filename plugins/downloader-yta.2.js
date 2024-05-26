f4cb21b5495f1c98f15bd75f3197f7c476c88681
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-26 22:04:55 +0300

    Update downloader-yta.2.js

--- plugins/downloader-yta.2.js
+++ plugins/downloader-yta.2.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, args}) => {
     const ttl = await yt.title;
     const size = await yt.audio[q].fileSizeH;
     const cap = `*◉—⌈📥 ♪ 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 ♪ 📥⌋—◉*\n❏ *العنوان:* ${ttl}\n❏ *الحجم:* ${size}\n`.trim();
-    await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'audio/mpeg', fileName: `${ttl}.mp3`}, {quoted: m});
+    await conn.sendMessage(m.chat, {document: {url: dl_url}, caption: cap, mimetype: 'audio/mp3', fileName: `${ttl}.mp3`}, {quoted: m});
   } catch {
     try {
       const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${args[0]}`);
@@ -20,7 +20,7 @@ const handler = async (m, {conn, args}) => {
       const n2 = lolh.result.link;
       const n3 = lolh.result.size;
       const cap2 = `*◉—⌈📥 ♪ 𝑴𝒊𝒓𝒛𝒂 𝑴𝒖𝒔𝒊𝒄 ♪ 📥⌋—◉*\n❏ *العنوان:* ${n}\n❏ *الحجم:* ${n3}\n✰`.trim();
-      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'audio/mpeg', fileName: `${n}.mp3`}, {quoted: m});
+      await conn.sendMessage(m.chat, {document: {url: n2}, caption: cap2, mimetype: 'audio/mp3', fileName: `${n}.mp3`}, {quoted: m});
     } catch {
       await conn.reply(m.chat, '*⚠️ Ocurrió un error*', m);
     }
