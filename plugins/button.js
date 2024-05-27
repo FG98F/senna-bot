52e4ccbfbfbbdfb6605c7f83557744c19ece6f11
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-27 17:16:22 +0300

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    conn.relayMessage(m.chat, {
+    conn.relayMessage(m.chat, image, {
         viewOnceMessage: {
             message: {
                 interactiveMessage: {
