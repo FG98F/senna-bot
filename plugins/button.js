f91472a7d2355a954fe58cc3ce995653f93b2c83
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-27 16:59:11 +0300

    Update button.js

--- plugins/button.js
+++ plugins/button.js
@@ -1,91 +1,49 @@
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.relayMessage(m.chat, {
-      viewOnceMessage: {
-        message: {
-          interactiveMessage: {
-            header: {
-              title: '*🎗️ قـائـمـة الـاوامــر🎗️*'
-            },
-            body: {
-              text: '🧿 افتح القائمة بواسطة الزر\n🍒 لا تلعب كثير في القائمة'
-            },
-            nativeFlowMessage: {
-              buttons: [
-                {
-                  name: 'single_select',
-                  buttonParamsJson: JSON.stringify({
-                    title: 'دوس عليا',
-                    sections: [
-                      {
-                        title: 'List',
-                        highlight_label: 'ON',
-                        rows: [
-                          {
-                            header: '☘️ قـسـم الـمطور',
-                            title: '.اوامر_المطور',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ قـسـم الـجروب',
-                            title: '.اوامر_الجروب',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ قـسـم الـتحميل',
-                            title: '.اوامر_التحميل',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ قـسـم الـترفيه',
-                            title: '.اوامر_الترفيه',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ قـسـم الـتحميل',
-                            title: '.اوامر_التحميل',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ قـسـم الـدين',
-                            title: '.اوامر_الدين',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: '☘️ كل الاوامر',
-                            title: '.كل_الاوامر',
-                            description: '',
-                            id: 'te'
-                          },
-                          {
-                            header: ' ☘️قـسـم الـصوتيات',
-                            title: '.اوامر_الصوتيات',
-                            description: '',
-                            id: 'te'
-                          }
+        viewOnceMessage: {
+            message: {
+                interactiveMessage: {
+                    header: {
+                        title: "🎗️ قائمة الأوامر 🎗️"
+                    },
+                    body: {
+                        text: "🧿 افتح القائمة بواسطة الزر\n🍒 لا تلعب كثيرًا في القائمة"
+                    },
+                    nativeFlowMessage: {
+                        buttons: [
+                            {
+                                name: "single_select",
+                                buttonParamsJson: JSON.stringify({
+                                    title: "دوس عليا",
+                                    sections: [
+                                        {
+                                            title: "☘️ قسم المطور",
+                                            highlight_label: "ON",
+                                            rows: [
+                                                {
+                                                    header: "☘️ قسم المطور",
+                                                    title: ".اوامر_المطور",
+                                                    description: "",
+                                                    id: "te"
+                                                },
+                                                // ... (add other sections here)
+                                            ]
+                                        }
+                                    ],
+                                    image: "https://i.ibb.co/1zdz2j3/logo.jpgs" // اضف رابط الصورة هنا
+                                }),
+                                messageParamsJson: ""
+                            }
                         ]
-                      }
-                    ],
-                    image: 'https://i.ibb.co/1zdz2j3/logo.jpgs' // اضف رابط الصورة هنا
-                  }),
-                  messageParamsJson: ''
+                    }
                 }
-              ]
-            },
-            image: 'https://i.ibb.co/1zdz2j3/logo.jpgs' // اضف رابط الصورة هنا أيضًا
-          }
+            }
         }
-      }
     }, {})
 }
 
-handler.help = ['info']
-handler.tags = ['main']
-handler.command = ['القائمة', 'القائمه']
+handler.help = ["info"]
+handler.tags = ["main"]
+handler.command = ["القائمة", "القائمه"]
 
 export default handler
