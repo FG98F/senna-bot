7bb081efa9cb407fb92ce905f9691c2c91fd3ce9
Master15921 <144853513+Master15921@users.noreply.github.com>
2024-05-28 17:25:38 +0300

    Update عين.js

--- plugins/عين.js
+++ plugins/عين.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat,  '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯' , conn.tebakbendera[id][0])
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch( https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/mark.json )).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/mark.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
@@ -16,7 +16,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 『ITACHI♦️BOT』
      `.trim()
     conn.tebakbendera[id] = [
-        await conn.sendFile(m.chat, json.img,   , caption, m),
+        await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
             if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
@@ -24,8 +24,8 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         }, timeout)
     ]
 }
-handler.help = [ 'guessflag' ]
-handler.tags = [ 'game' ]
+handler.help = ['guessflag']
+handler.tags = ['game']
 handler.command = /^عين/i
 
 export default handler
