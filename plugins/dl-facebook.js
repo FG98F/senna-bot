c1ee23421fe68fb596628f7b0d7624246bf6a3a6
FG98 <81941690+FG98F@users.noreply.github.com>
2024-07-23 14:25:43 -0300

    Update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -3,23 +3,21 @@ import fg from 'api-dylux';
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
   
 if (!args[0]) throw `✳️ ${mssg.noLink('Facebook')}\n\n📌 ${mssg.example} :\n*${usedPrefix + command}* https://fb.watch/d7nB8-L-gR/`
-  m.react(rwait);
+  m.react(rwait)
 
   try {
-    let result = await fg.fbdl(args[0]);
-    let tex = `
-┌─⊷ *FBDL*
-▢ *${mssg.title}:* ${result.title}
-└───────────`;
-    conn.sendFile(m.chat, result.videoUrl, 'fb.mp4', tex, m);
-    m.react(done);
+    let res = await fetch(global.API('fgmods', '/api/downloader/fbdl', { url: args[0] }, 'apikey'))
+    let data = await res.json()
+    
+    conn.sendFile(m.chat, data.result.HD, 'fb.mp4', `✅ ${mssg.result}`, m)
+    m.react(done)
   } catch (error) {
     m.reply(mssg.error)
   }
-};
-handler.help = ['facebook'].map(v => v + ' <url>');
-handler.tags = ['dl'];
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i;
-handler.diamond = true;
+}
+handler.help = ['facebook'].map(v => v + ' <url>')
+handler.tags = ['dl']
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+handler.diamond = true
 
-export default handler;
+export default handler
