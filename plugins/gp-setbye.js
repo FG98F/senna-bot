162fe3d56db9845fe72f73a3d92e1a8bdf74b778
FG98 <81941690+FG98F@users.noreply.github.com>
2024-08-18 20:59:44 -0300

    Update gp-setbye.js

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -3,7 +3,7 @@
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sBye = text
-    m.reply(`✅ ${msg.leaMsgOn}`)
+    m.reply(`✅ ${mssg.leaMsgOn}`)
   } else throw `✳️ ${mssg.leaMsg}`
 }
 handler.help = ['setbye <text>']
